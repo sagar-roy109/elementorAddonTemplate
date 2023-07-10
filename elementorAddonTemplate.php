8a3d5a0ba762d0968dd173755d8eef670ac8b312
sagar-roy109 <sagar.roy.freelancer@gmail.com>
2023-07-10 10:12:41 -0600

    Did initial setup

---
+++ elementorAddonTemplate.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Elementor Test Addon
+ * Description: Custom Elementor addon.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.0.0
+ * Author:      Elementor Developer
+ * Author URI:  https://developers.elementor.com/
+ * Text Domain: elementor-test-addon
+ * 
+ * Elementor tested up to: 3.7.0
+ * Elementor Pro tested up to: 3.7.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+function elementor_test_addon() {
+
+	// Load plugin file
+	require_once( __DIR__ . '/includes/plugin.php' );
+
+	// Run the plugin
+	\Elementor_Test_Addon\Plugin::instance();
+
+}
+add_action( 'plugins_loaded', 'elementor_test_addon' );
